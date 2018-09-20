46171e594de4cb70b1a4b1c1b4ea71fa7275102f
shimbuichi <contact.shimbuichi@gmail.com>
2018-09-21 07:54:52 +0900

    php勉強用

---
+++ src/detail.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8"/>
+    <title> 商品詳細</title>
+    <link rel="stylesheet" type="text/css" href="/static/css/style.css">
+</head>
+<body>
+<header class="site-header">
+    <div class="site-title">Freitech</div>
+    <nav>
+        <ul class="menu">
+            <li><a href="index.php">商品一覧</a></li>
+            <li><a href="detail.php">商品詳細</a></li>
+            <li><a href="index.html">next</a></li>
+        </ul>
+        <ul class="user-management">
+            <li><a href="index.html">ログイン</a></li>
+            <li><a href="index.html">ログアウト</a></li>
+        </ul>
+    </nav>
+</header>
+<div class="site-content">
+    <section class="product-container">
+        <div class="product-title"><span class="badge">スマホ</span>iPhone6s</div>
+        <div class="product-content-detail">
+            <img class="product-detail-img" src="static/image/iPhone_color.jpg">
+        </div>
+        <div class="product-content-list">
+            <ul class="product-list">
+                <li>
+                    <img class="product-detail-img" src="static/image/iPhone_color.jpg">
+                </li>
+                <li>
+                    <img class="product-detail-img" src="static/image/iPhone6s_overview.jpg">
+                </li>
+                <li>
+                    <img class="product-detail-img" src="static/image/iPhone6s.jpg">
+                </li>
+            </ul>
+        </div>
+        <div class="clear product-detail">
+            <p>
+                texttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttext
+                texttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttext
+                texttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttext
+                texttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttext
+            </p>
+        </div>
+        <div class="product-action">
+            <a class="back-list" href="#">商品一覧に戻る</a>
+            <div class="action-area">
+                <span class="price">\85,000-</span>
+                <button type="button" class="button-buy">買う</button>
+            </div>
+        </div>
+    </section>
+</div>
+<footer>
+    <div class="copyright">Copyright freitech.</div>
+</footer>
+
+</body>
+</html>
